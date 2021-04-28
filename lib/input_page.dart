976c8552601a253f326df3d5a5c6c9f11e710c8a
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-04-28 21:44:15 +0530

    ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomBtnHeight = 80.0;
+const activeColor = Color(0xFF1D1E33);
+const bottomBtnColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,12 +17,87 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                  child: ReusableCard(
+                colour: activeColor,
+                cardchild: IconContent(FontAwesomeIcons.mars, 'MALE'),
+              )),
+              Expanded(
+                  child: ReusableCard(
+                colour: activeColor,
+                cardchild: IconContent(FontAwesomeIcons.venus, 'FEMALE'),
+              )),
+            ],
+          )),
+          Expanded(child: ReusableCard(colour: activeColor)),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(child: ReusableCard(colour: activeColor)),
+              Expanded(child: ReusableCard(colour: activeColor)),
+            ],
+          )),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: bottomBtnColor,
+            width: double.infinity,
+            height: bottomBtnHeight,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  IconContent(this.icon, this.text);
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            color: Color(0xFF8D8E98),
+            fontSize: 18.0,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardchild});
+  final Color colour;
+  final Widget cardchild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
       ),
+      child: cardchild,
     );
   }
 }
