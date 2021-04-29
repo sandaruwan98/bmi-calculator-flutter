f69c132aad735a4dc7d0d7e2bec6e09eb819b425
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-04-29 12:55:22 +0530

    bottom btn

---
+++ lib/bottom_btn.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'const.dart';
+
+class BottomBtn extends StatelessWidget {
+  const BottomBtn({@required this.text, @required this.ontap});
+
+  final Function ontap;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ontap(),
+      child: Container(
+        margin: EdgeInsets.only(top: 10.0),
+        color: kBottomBtnColor,
+        width: double.infinity,
+        height: kBottomBtnHeight,
+        child: Center(
+          child: Text(
+            text,
+            style: klargeBtnTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
